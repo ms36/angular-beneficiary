86b413d3eca562658ee1910827e0c98128a4678c
ms36 <mstevenson36@hotmail.com>
2020-07-09 09:54:38 -0400

    deleted list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
+import { NgModule } from '@angular/core';
 import { MaterialModule } from './material';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {OverlayModule} from '@angular/cdk/overlay';
+
+import { AppComponent } from './app.component';
 import { BeneficiaryComponent } from './components/beneficiary/beneficiary.component';
-import { BeneficiaryListComponent } from './components/beneficiary-list/beneficiary-list.component';
 import { BeneficiaryAppComponent } from './components/beneficiary-app/beneficiary-app.component';
 import { BeneficiaryDetailsComponent } from './components/beneficiary-details/beneficiary-details.component';
 import { BeneficiaryHeaderComponent } from './components/beneficiary-header/beneficiary-header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     BeneficiaryComponent,
-    BeneficiaryListComponent,
     BeneficiaryAppComponent,
     BeneficiaryDetailsComponent,
-    BeneficiaryHeaderComponent
+    BeneficiaryHeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    OverlayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/beneficiary-app/beneficiary-app.component.ts
+++ src/app/components/beneficiary-app/beneficiary-app.component.ts
@@ -13,33 +13,44 @@ export class BeneficiaryAppComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.user = {
-    id: 1,
-    email: 'email@example.com',
-    firstName: 'Bob',
-    lastName: 'Ross',
-    primaryBeneficiaries: [
-      {
+      this.user = {
       id: 1,
-      firstName: 'Bruce',
-      lastName: 'Wayne',
-      percentOfBenefit: 50
-    },
-    {
-      id: 2,
-      firstName: 'John',
-      lastName: 'Wayne',
-      percentOfBenefit: 50
-    }
-  ],
-    contingentBeneficiaries: []
-  };
+      email: 'email@example.com',
+      firstName: 'Bob',
+      lastName: 'Ross',
+      primaryBeneficiaries: [
+        {
+        id: 1,
+        firstName: 'Bruce',
+        lastName: 'Wayne',
+        percentOfBenefit: 50
+      },
+      {
+        id: 2,
+        firstName: 'John',
+        lastName: 'Wayne',
+        percentOfBenefit: 50
+      }
+    ],
+      contingentBeneficiaries: []
+    };
   }
 
   deleteBeneficiary(beneficiary: Beneficiary) {
     console.log('app-beneficiary-app: delete');
     this.user.primaryBeneficiaries = this.user.primaryBeneficiaries.filter(b => b.id !== beneficiary.id);
     // this.beneficiaryService.deleteTodo(beneficiary).subscribe();
+  }
+
+  addBeneficiary() {
+    const newBeneficiary = {
+      id: 3,
+      firstName: 'Lil',
+      lastName: 'Wayne',
+      percentOfBenefit: 50
+    };
+
+    this.user.primaryBeneficiaries.push(newBeneficiary);
   }
 
 }
